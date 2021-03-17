10a763196b819d6c4c11cab8dcf6309a779e6f29
Natalia Antonina Dzieciol <naawiedzona.93@gmail.com>
2021-03-17 15:20:38 +0100

    vue-create components

---
+++ src/components/Hijo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mensaje">
+        <p>{{mensaje}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            mensaje: 'Soy el hijo'
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/components/Padre.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mensaje">
+        <p>{{mensaje}}</p>
+        <Hijo></Hijo>
+    </div>
+</template>
+
+<script>
+import Hijo from './Hijo'
+
+export default {
+    data () {
+        return {
+            mensaje: 'Soy el padre'
+        }
+    },
+    components: {
+        Hijo
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
